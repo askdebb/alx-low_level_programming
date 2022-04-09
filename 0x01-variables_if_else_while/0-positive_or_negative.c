b4fa9727e5afc4e3897610fbceac2469da75790c
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-08 17:45:15 -0700

    worked on betty related issues #10

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,7 @@
 
 /**
  * main - Print a random number and states whether
- * 	  it is positive, negative, or zero.
- *
+ * it is positive, negative, or zero.
  * Return: Always 0. 
  */
 int main(void)
