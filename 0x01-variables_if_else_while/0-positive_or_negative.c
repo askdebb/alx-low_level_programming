5336411ccfae77cd429734918788e15d3bd352a3
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-08 17:24:26 -0700

    worked on betty related issues #5

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main program entry starts here
- * Return: Always 0 
- */
+/*
+  main program entry starts here
+  Return: Always 0 
+*/
 int main(void)
 {
 	int n;
