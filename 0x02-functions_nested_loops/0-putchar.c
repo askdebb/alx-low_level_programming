d79371e6d13a438d6d2b78c8b3e4b2a2a86400f8
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-12 14:09:27 -0700

    modify #2 on '.c' file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * main - entry point of the program
- *	takes in a single character
- *	and put it to stdout
+ *
+ *	takes in a single character in a word
+ *
+ *	and puts it to stdout
+ *
  * Return: 0 success.
  */
 
 int main(void)
 {
-	int i;
-	i = 0;
 	char word[9] = "_putchar";
+	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
