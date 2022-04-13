d6c0627766c29bcd699e993e895187c9ab101fb0
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-13 15:31:45 -0700

    modified largest.c file #4

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,13 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > b)
+	else if (c > b && c > a)
 	{
 		largest = c;
 	{
 	else
 	{
-		largest = b;
+		printf("Kindly check your numbers");
 	}
 	return (largest);
 }
