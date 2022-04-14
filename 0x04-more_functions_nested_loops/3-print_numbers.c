95c56370b0390151e07418b38e93287dffb440d9
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-14 12:48:21 -0700

    _putchar used successfully with print_numbers()

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * _putchar - displays the input
+ */
+
+void print_numbers(void)
+{
+	int numStarts_0, numFinishes_9;
+
+	numFinishes_9 = '9';
+
+	for (numStarts_0 = '0'; numStarts_0 <= numFinishes_9; numStarts_0++)
+	{
+		_putchar(numStarts_0);
+	}
+	_putchar('\n');
+}
