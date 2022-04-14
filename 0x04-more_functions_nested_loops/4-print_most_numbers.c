7a9ae01ac50cf6063a58d3b12ba835bb368c7235
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-14 14:44:25 -0700

    printed most numbers from 0 -9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - excludes some numbers with
+ *
+ * an if condition which are 2 and 4
+ *
+ * _putchar - displays the results to the stdout
+ *
+ * Return: 0 as success.
+ */
+
+void print_most_numbers(void)
+{
+	int startNum_0;
+	
+	startNum_0 = '0';
+
+	for (; startNum_0 <= '9'; startNum_0++)
+	{
+		if (startNum_0 != '2' && startNum_0 != '4')
+		{
+			_putchar(startNum_0);
+		}
+		_putchar('\n');
+	}
+}
