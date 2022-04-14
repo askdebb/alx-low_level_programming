50cca59ae1677028903b18a9ac17a8c0d2a50f9b
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-04-14 11:13:51 -0700

    function for mul() created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - takes two parameters and find their products
+ * @a: first int input
+ * @b: second int input
+ * Return: the product of int a and int b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
