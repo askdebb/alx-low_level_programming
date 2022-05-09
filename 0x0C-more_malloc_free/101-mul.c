0a3b4cefa326604569308485813da190be8a2e97
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-05-09 16:46:46 -0700

    mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,7 +15,7 @@ int num1, num2;
 	exit(98); }
 	for (num1 = 1; num1 < argc; num1++)
 	{
-		for (j = 0; argv[num1][num2] != '\0'; num2++)
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
 			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
 			{  printf("Error\n");
